720d5c713e0db848cef2fbf4e880070c59646a66
ErickCristhian <30393905+ErickCristhian@users.noreply.github.com>
2020-09-22 22:35:45 -0300

    App Arrocha

---
+++ JogoDoArrocha/app/src/main/java/com/ifpb/pdm/jogodoarrocha/GanhouActivity.kt
@@ -0,0 +1,21 @@
+package com.example.jogodoarrocha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.ifpb.pdm.jogodoarrocha.R
+
+class GanhouActivity : AppCompatActivity() {
+    private lateinit var tvResultado: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ganhou)
+
+        this.tvResultado = findViewById(R.id.tvGanhouResultado)
+
+        this.tvResultado.setOnClickListener{
+            finish()
+        }
+    }
+}
---
+++ JogoDoArrocha/app/src/main/java/com/ifpb/pdm/jogodoarrocha/PerdeuActivity.kt
@@ -0,0 +1,21 @@
+package com.example.jogodoarrocha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.ifpb.pdm.jogodoarrocha.R
+
+class PerdeuActivity : AppCompatActivity() {
+    private lateinit var tvResultado: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perdeu)
+
+        this.tvResultado = findViewById(R.id.tvPerdeuResultado)
+
+        this.tvResultado.setOnClickListener{
+            finish()
+        }
+    }
+}
